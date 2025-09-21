5420ba08bd017cb202bf9fef76b1b3fc18a9be06
PedroSmaxY <pedrohnovais@outlook.com>
2025-09-21 17:37:04 -0300

    Registrando rotas de autenticação

--- src/server.ts
+++ src/server.ts
@@ -4,6 +4,7 @@ import { PrismaClient } from '@prisma/client';
 import { lessonRoutes } from '@/routes/lesson-routes.js';
 import { teacherRoutes } from '@/routes/teacher-routes.js';
 import { studentRoutes } from '@/routes/student-routes.js';
+import { authRoutes } from './routes/auth-routes.js';
 
 export const db = new PrismaClient();
 const server = fastify({ logger: true });
@@ -14,6 +15,7 @@ await server.register(cors, { origin: '*' });
 await server.register(lessonRoutes, { prefix: '/lessons' });
 await server.register(teacherRoutes, { prefix: '/teachers' });
 await server.register(studentRoutes, { prefix: '/students' });
+await server.register(authRoutes, { prefix: '/auth' });
 
 server.get('/', async (request, reply) => {
   reply.code(200).send({ hello: 'world' });
